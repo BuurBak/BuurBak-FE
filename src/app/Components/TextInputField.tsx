f13d6bfba44797bab4a1bad9e22632a2d336d1a3
segervvv <seger.vanvlijmen@student.hu.nl>
2024-04-08 11:51:11 +0200

    Inputfield made

--- src/app/Components/TextInputField.tsx
+++ src/app/Components/TextInputField.tsx
@@ -1,24 +1,24 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 type ButtonType = {
   label: string,
   outline?: boolean,
-  icon?: "MagnifyingGlass",
-  iconPosition?: "right" | "left";
+  icon?: boolean,
+  inputValue: string,
+  setInputValue: any,
+  filled?: boolean;
 };
 
-const TextInputField = ({ label, outline, icon, iconPosition, ...props }: ButtonType) => {
-  const importIconString = icon + "Icon";
+const TextInputField = ({ label, outline, icon, inputValue, setInputValue, filled, ...props }: ButtonType) => {
+
   return (
-    <>
-@if ({icon} != null ) {
-          <MagnifyingGlassIcon />
-        }
-      <input aria-label={label} className={outline ? "outline-primary-100" : ""} placeholder={label}>
-        
+    <div className="relative w-fit my-2 ml-10">
+      <input value={inputValue} onChange={setInputValue} className={(outline ? "outline-0 border border-primary-100" : "outline-0 border border-offWhite-100") + " h-12 focus:outline-0 px-3 rounded"} placeholder={label}>
       </input>
-    </>
-  );
+      {icon && <div className={(filled ? "right-0 top-0 p-2 bg-primary-100 rounded-r" : "right-2 top-2") + " absolute"}><MagnifyingGlassIcon className={(filled ? "text-white" : "") + " h-8 w-8 "} /></div>}
+    </div>
+  )
 };
+
 
 export default TextInputField;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+"use client"
+
+import { useState } from "react";
 import Footer from "./Components/Footer";
 import Highlights from "./Components/Highlights";
 import TextInputField from "./Components/TextInputField";
 
 export default function Home() {
+
+  const [inputValue, setInputValue] = useState('');
+
+  const changeInputValue = (event: { target: { value: any; }; }) => {
+    if (event.target && event.target.value !== undefined) {
+      setInputValue(event.target.value)
+    }
+  }
+
   return (
     <div>
-      <TextInputField label="test" outline/>
+      <TextInputField label="Type" outline filled icon inputValue={inputValue} setInputValue={changeInputValue} />
+      <p>Input Value: {inputValue}</p>
       <Highlights />
       <Footer />
     </div>
