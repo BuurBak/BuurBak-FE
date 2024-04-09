3a2de60662cc6998416dd8f4d147a08bb618a21f
segervvv <seger.vanvlijmen@student.hu.nl>
2024-04-09 12:38:35 +0200

    Cursor not allowed

--- src/app/Components/Button.tsx
+++ src/app/Components/Button.tsx
@@ -15,7 +15,7 @@ const Button = ({ label, styling, icon, disabled, buttonAction, ...props }: Butt
       <button
         onClick={buttonAction}
         type="button"
-        className={(styling !== undefined ? styling : "") + (disabled ? " bg-gray-100 cursor-default" : " bg-primary-100 hover:bg-primary-200") + " flex flex-row justify-center items-center px-7 h-12 font-bold rounded-sm text-white"}
+        className={(styling !== undefined ? styling : "") + (disabled ? " bg-gray-100 cursor-not-allowed" : " bg-primary-100 hover:bg-primary-200") + " flex flex-row justify-center items-center px-7 h-12 font-bold rounded-sm text-white"}
       >
         {icon && <MagnifyingGlassIcon className="w-8 h-8 mr-2"/>}
         {label}
