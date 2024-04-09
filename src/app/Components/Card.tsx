c37654f1a192643edfd882ccfb27a34db726ec7f
segervvv <seger.vanvlijmen@student.hu.nl>
2024-04-09 14:50:39 +0200

    WIP

--- src/app/Components/Card.tsx
+++ src/app/Components/Card.tsx
@@ -2,13 +2,17 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { ChangeEventHandler } from "react";
 
 type CardType = {
-  label?: string;
+  label?: string,
+  type?: "overview" | "category";
 };
 
-const Card = ({ label, ...props }: CardType) => {
+const Card = ({ label, type, ...props }: CardType) => {
 
   return (
-    <div className="">
+    <div className={(type === "overview" ? "w-64 h-80 border border-offWhite-100" : "")}>
+      <div className="w-full h-44">
+      <img alt="trailer" src={allImages[currentImageIndex]} />
+      </div>
       </div>
   )
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   return (
     <div>
-      <Card/>
+      <Card type="overview"/>
       <Landing />
       <Highlights />
       <Footer />
