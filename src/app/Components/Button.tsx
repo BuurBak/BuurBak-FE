16975d1647069cedf05ff6651d0b71634d9157ca
segervvv <seger.vanvlijmen@student.hu.nl>
2024-04-09 11:10:12 +0200

    WIP

--- src/app/Components/Button.tsx
+++ src/app/Components/Button.tsx
@@ -1,15 +1,20 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
 type ButtonType = {
   label: string,
-  styling?: string;
+  styling?: string,
+  icon?: boolean,
+  disabled?: boolean;
 };
 
-const Button = ({ label, styling, ...props }: ButtonType) => {
+const Button = ({ label, styling, icon, disabled, ...props }: ButtonType) => {
   return (
     <>
       <button
         type="button"
-        className={(styling !== undefined ? styling : "") + " px-7 h-12 font-bold bg-primary-100 rounded-sm text-white"}
+        className={(styling !== undefined ? styling : "") + (disabled ? "bg-gray-100 hover:bg-gray-100" : "") + " flex flex-row justify-center items-center px-7 h-12 font-bold bg-primary-100 hover:bg-primary-200 rounded-sm text-white"}
       >
+        {icon && <MagnifyingGlassIcon className="w-8 h-8 mr-2"/>}
         {label}
       </button>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-"use client"
-
-import { useState } from "react";
+import Button from "./Components/Button";
 import Footer from "./Components/Footer";
 import Highlights from "./Components/Highlights";
-import TextInputField from "./Components/TextInputField";
 import Landing from "./Components/Landing";
 
 export default function Home() {
 
   return (
     <div>
+      <Button label='Bekijk het aanbod' icon disabled/>
       <Landing />
       <Highlights />
       <Footer />
