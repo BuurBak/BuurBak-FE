46a1df626dcdbe1fb9b9eec4a765c4a8c5b5fa8b
Luuk van Dijk <luukvandijk2003@gmail.com>
2024-06-11 17:59:19 +0200

    added registreren

--- src/app/Components/Register.tsx
+++ src/app/Components/Register.tsx
@@ -4,8 +4,11 @@ import Button from "./Button";
 import InputField from "./InputField";
 
 const Register = () => {
+  const [hasAccount, setHasAccount] = useState(false);
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [name, setName] = useState();
+  const [mobile, setMobile] = useState();
 
   const handleSubmit = () => {
     let data = {
@@ -21,6 +24,20 @@ const Register = () => {
       className="w-full flex flex-col gap-4 pb-4"
       onSubmit={() => handleSubmit()}
     >
+      {hasAccount && (
+        <div className="w-full">
+          <label>Naam</label>
+          <InputField
+            setInputValue={setName}
+            type="text"
+            className="!w-full"
+            label="Naam"
+            inputType="text"
+            outline={true}
+            required={hasAccount}
+          />
+        </div>
+      )}
       <div className="w-full">
         <label>Email</label>
         <InputField
@@ -33,6 +50,20 @@ const Register = () => {
           required={true}
         />
       </div>
+      {hasAccount && (
+        <div className="w-full">
+          <label>Telefoon nummer</label>
+          <InputField
+            setInputValue={setMobile}
+            type="number"
+            className="!w-full"
+            label="Telefoon nummer"
+            inputType="text"
+            outline={true}
+            required={hasAccount}
+          />
+        </div>
+      )}
       <div>
         <label>Wachtwoord</label>
         <InputField
@@ -45,16 +76,29 @@ const Register = () => {
           required={true}
         />
       </div>
-      <Button label="Sign in" submit={true} />
-      <p>
-        Nog geen BuurBak account?{" "}
-        <span
-          className="text-primary-100"
-          onClick={() => console.log("Registeren")}
-        >
-          Registreren
-        </span>
-      </p>
+      <Button label={hasAccount ? "Registreer" : "Log in"} submit={true} />
+      {!hasAccount && (
+        <p>
+          Nog geen BuurBak account?{" "}
+          <span
+            className="text-primary-100"
+            onClick={() => setHasAccount(true)}
+          >
+            Registreren
+          </span>
+        </p>
+      )}
+      {hasAccount && (
+        <p>
+          Heb je al een account?{" "}
+          <span
+            className="text-primary-100"
+            onClick={() => setHasAccount(false)}
+          >
+            Inloggen
+          </span>
+        </p>
+      )}
     </form>
   );
 };
