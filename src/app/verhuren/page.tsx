9462fd06ae550dd2af1ca21fb937149ce9235e66
Glenn van der Linden <39535955+DevGlenn@users.noreply.github.com>
2024-11-19 15:40:08 +0100

    foto's gemaakt en setup stappenplan
    Co-authored-by: Luuk van Dijk <Luukvdijk@users.noreply.github.com>

--- src/app/verhuren/page.tsx
+++ src/app/verhuren/page.tsx
@@ -1,7 +1,25 @@
+import { ImageIcon, ImagesIcon } from "lucide-react";
 import React from "react";
 
 const verhuren = () => {
-  return <div>verhuren</div>;
+  return (
+    <div className="w-full h-dvh flex flex-row pt-20">
+      <div className="w-2/3 h-full">
+        <h1 className="text-center">Maak jouw advertentie compleet</h1>
+        <hr className="w-full h-1 bg-black-200" />
+
+        <div className="flex flex-row items-center justify-start gap-6 rounded border-2 mt-5 p-6">
+          <ImagesIcon className="size-9" />
+          <div className="flex flex-col">
+            <p>Sleep je foto's hierheen</p>
+            <p className="underline">Upload tenminste 4 foto's</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-1/3 h-full bg-offWhite-100"></div>
+    </div>
+  );
 };
 
 export default verhuren;
