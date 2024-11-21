629156263b07d5b9d7da56f50c4a7226593f93b4
Glenn van der Linden <39535955+DevGlenn@users.noreply.github.com>
2024-11-21 16:01:03 +0100

    fixed spacing text en size text area

--- src/app/verhuren/page.tsx
+++ src/app/verhuren/page.tsx
@@ -1,59 +1,65 @@
 import { ImageIcon, ImagesIcon } from "lucide-react";
 import React from "react";
+import InputField from "../Components/InputField";
 
 const verhuren = () => {
   return (
     <div className="w-full h-dvh flex flex-row pt-20 gap-5">
       <div className="w-2/3 h-full">
-        <h1 className="text-center text-h3">Maak jouw advertentie compleet</h1>
-        <hr className="w-3/4 h-1 bg-black-200" />
+        <h1 className="text-center text-h3 font-bold">
+          Maak jouw advertentie <br /> compleet
+        </h1>
+        <div className="flex flex-col mt-5 items-center">
+          <hr className="w-11/12 h-1 bg-black-200 " />
+        </div>
         <div className="flex flex-col items-center pt-5 gap-5">
-          <p className="font-bold">
-            Kies de foto's die jouw aanhanger het beste representeren:
-          </p>
-          <div className="w-1/2 flex flex-row justify-start items-start gap-5 rounded border-2 p-6">
-            <ImagesIcon className="size-9" />
-            <div className="flex flex-col">
-              <p>Sleep je foto's hierheen</p>
-              <p className="underline">Upload tenminste 4 foto's</p>
+          <div className="w-3/4 flex flex-col gap-5">
+            <p className="font-bold">
+              Kies de foto's die jouw aanhanger het beste representeren:
+            </p>
+            <div className="w-full flex flex-row items-start gap-5 rounded border-2 p-6 pl-10">
+              <ImagesIcon className="size-9" />
+              <div className="flex flex-col">
+                <p>Sleep je foto's hierheen</p>
+                <p className="underline">Upload tenminste 4 foto's</p>
+              </div>
             </div>
           </div>
-          <div>
-            <p className="font-bold">Kies je soort aanhanger:</p>
+          <div className="w-3/4">
+            <p className="font-bold ">Kies je soort aanhanger:</p>
           </div>
-          <div className="">
+          <div className="w-3/4">
             <p className="font-bold">Geef een beschrijving voor de huurder:</p>
             <textarea
               id="message"
-              rows="4"
-              className="flex flex-row mt-5 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border-2 border-orange-300  focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-orange-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="flex flex-row mt-5 p-2.5 w-full h-32 rounded border-2 border-primary-100"
               placeholder="Typ een kleine beschrijving over je aanhanger..."
-            ></textarea>
+            />
           </div>
-          <div>
+          <div className="w-3/4">
             <p className="font-bold">
               Kies de accesoires die je bij je aanhanger wilt verhuren:
             </p>
           </div>
-          <div>
+          <div className="w-3/4">
             <p className="font-bold">
               Kies de locatie waar je je aanhanger vanaf verhuurd:
             </p>
           </div>
-          <div>
+          <div className="w-3/4">
             <p className="font-bold">
               Kies het soort rijbewijs wat vereist is:
             </p>
           </div>
-          <div>
+          <div className="w-3/4">
             <p className="font-bold">Vul de afmetingen van je aanhanger in:</p>
           </div>
-          <div>
+          <div className="w-3/4">
             <p className="font-bold">
               Voor hoeveel € wil je je aanhanger verhuren:
             </p>
           </div>
-          <div>
+          <div className="w-3/4">
             <p className="font-bold">
               Kies de dagen waarop je de aanhanger beschikbaar wilt maken voor
               ophaal:
