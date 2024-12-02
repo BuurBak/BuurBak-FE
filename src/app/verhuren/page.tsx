bc2f5ea2a93c9dadc8e2f76b34d53cce6012be93
Glenn van der Linden <39535955+DevGlenn@users.noreply.github.com>
2024-12-02 13:24:39 +0100

    dagen afgemaakt, verder research over e weghalen uit cijfer boxes en dropdown
    Co-authored-by: Luuk van Dijk <Luukvdijk@users.noreply.github.com>

--- src/app/verhuren/page.tsx
+++ src/app/verhuren/page.tsx
@@ -1,16 +1,28 @@
-import { ImageIcon, ImagesIcon } from "lucide-react";
-import React from "react";
+"use client";
+
+import { Check, ImageIcon, ImagesIcon, X } from "lucide-react";
+import React, { useState } from "react";
 import InputField from "../Components/InputField";
 
 const verhuren = () => {
+  const [day, setDay] = useState({
+    monday: false,
+    tuesday: false,
+    wednesday: false,
+    thursday: false,
+    friday: false,
+    saturday: false,
+    sunday: false,
+  });
+
   return (
-    <div className="w-full h-fit flex flex-row pt-20 gap-5">
+    <div className="w-full min-h-screen h-fit flex flex-col sm:flex-row pt-20 gap-5">
       <div className="w-2/3">
         <h1 className="text-center text-h3 font-bold">
-          Maak jouw advertentie <br /> compleet
+          Maak jouw advertentie compleet
         </h1>
-        <div className="flex flex-col mt-5 items-center">
-          <hr className="w-11/12 h-1 bg-black-200 " />
+        <div className="flex flex-col mt-5 items-center ">
+          <hr className="w-11/12 h-0.5 bg-black-200 " />
         </div>
         <div className="flex flex-col items-center pt-5 gap-5">
           <div className="w-3/4 flex flex-col gap-5">
@@ -59,6 +71,7 @@ const verhuren = () => {
                 label="Vul de lengte van je aanhanger in "
                 icon
                 iconLeft
+                type="number"
                 iconName="L"
                 outline
                 className="w-full"
@@ -67,6 +80,7 @@ const verhuren = () => {
                 inputType="text"
                 label="Vul de breedte van je aanhanger in "
                 icon
+                type="number"
                 iconLeft
                 iconName="B"
                 outline
@@ -76,6 +90,7 @@ const verhuren = () => {
                 inputType="text"
                 label="Vul de hoogte van je aanhanger in "
                 icon
+                type="number"
                 iconLeft
                 iconName="H"
                 outline
@@ -95,17 +110,129 @@ const verhuren = () => {
               iconName="Euro"
               outline
               className="w-full"
+              type="number"
             />
           </div>
-          <div className="w-3/4">
+          <div className="flex flex-col gap-5 w-3/4">
             <p className="font-bold">
               Kies de dagen waarop je de aanhanger beschikbaar wilt maken voor
               ophaal:
             </p>
+            <div className="flex flex-row gap-3 w-full">
+              {/* monday */}
+              <div
+                className={`flex flex-col rounded w-fit p-6 cursor-pointer ${
+                  day.monday ? "bg-primary-100 text-white" : "bg-offWhite-100"
+                }`}
+                onClick={() =>
+                  setDay((prevDay) => ({
+                    ...prevDay,
+                    monday: !prevDay.monday,
+                  }))
+                }
+              >
+                <p className="font-bold">Ma</p>
+
+                {day.monday ? <Check /> : <X />}
+              </div>
+              {/* tuesday */}
+              <div
+                className={`flex flex-col rounded w-fit p-6 cursor-pointer ${
+                  day.tuesday ? "bg-primary-100 text-white" : "bg-offWhite-100"
+                }`}
+                onClick={() =>
+                  setDay((prevDay) => ({
+                    ...prevDay,
+                    tuesday: !prevDay.tuesday,
+                  }))
+                }
+              >
+                <p className="font-bold">Di</p>
+
+                {day.tuesday ? <Check /> : <X />}
+              </div>
+              {/* wednesday */}
+              <div
+                className={`flex flex-col rounded w-fit p-6 cursor-pointer ${
+                  day.wednesday
+                    ? "bg-primary-100 text-white"
+                    : "bg-offWhite-100"
+                }`}
+                onClick={() =>
+                  setDay((prevDay) => ({
+                    ...prevDay,
+                    wednesday: !prevDay.wednesday,
+                  }))
+                }
+              >
+                <p className="font-bold">Wo</p>
+                {day.wednesday ? <Check /> : <X />}
+              </div>
+              {/* thursday */}
+              <div
+                className={`flex flex-col rounded w-fit p-6 cursor-pointer ${
+                  day.thursday ? "bg-primary-100 text-white" : "bg-offWhite-100"
+                }`}
+                onClick={() =>
+                  setDay((prevDay) => ({
+                    ...prevDay,
+                    thursday: !prevDay.thursday,
+                  }))
+                }
+              >
+                <p className="font-bold">Do</p>
+                {day.thursday ? <Check /> : <X />}
+              </div>
+              {/* friday */}
+              <div
+                className={`flex flex-col rounded w-fit p-6 cursor-pointer ${
+                  day.friday ? "bg-primary-100 text-white" : "bg-offWhite-100"
+                }`}
+                onClick={() =>
+                  setDay((prevDay) => ({
+                    ...prevDay,
+                    friday: !prevDay.friday,
+                  }))
+                }
+              >
+                <p className="font-bold">Vr</p>
+                {day.friday ? <Check /> : <X />}
+              </div>
+              {/* saturday */}
+              <div
+                className={`flex flex-col rounded w-fit p-6 cursor-pointer ${
+                  day.saturday ? "bg-primary-100 text-white" : "bg-offWhite-100"
+                }`}
+                onClick={() =>
+                  setDay((prevDay) => ({
+                    ...prevDay,
+                    saturday: !prevDay.saturday,
+                  }))
+                }
+              >
+                <p className="font-bold">Za</p>
+                {day.saturday ? <Check /> : <X />}
+              </div>
+              {/* sunday */}
+              <div
+                className={`flex flex-col rounded w-fit p-6 cursor-pointer ${
+                  day.sunday ? "bg-primary-100 text-white" : "bg-offWhite-100"
+                }`}
+                onClick={() =>
+                  setDay((prevDay) => ({
+                    ...prevDay,
+                    sunday: !prevDay.sunday,
+                  }))
+                }
+              >
+                <p className="font-bold">Zo</p>
+                {day.sunday ? <Check /> : <X />}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div className="w-1/3 bg-offWhite-100"></div>
+      <div className="w-1/3 bg-offWhite-100 min-h-screen"></div>
     </div>
   );
 };
