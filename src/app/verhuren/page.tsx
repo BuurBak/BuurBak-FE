0b4c2399e4d927bec32d5f32803ecf301c136e90
Glenn van der Linden <39535955+DevGlenn@users.noreply.github.com>
2024-12-14 14:40:24 +0100

    beetje in de war over background size, meerdere classes gebruikt maar niet heel veel wijzer

--- src/app/verhuren/page.tsx
+++ src/app/verhuren/page.tsx
@@ -63,10 +63,10 @@ const Verhuren = () => {
   return (
     <div className="w-full min-h-screen h-fit flex flex-col sm:flex-row pt-20 gap-5">
       <div className="w-2/3">
-        <h1 className="text-center text-h3 font-bold">
+        <h1 className="text-center text-h3 font-bold mt-2">
           Maak jouw advertentie compleet
         </h1>
-        <div className="flex flex-col mt-5 items-center ">
+        <div className="flex flex-col mt-5 items-center gap-5">
           <hr className="w-11/12 h-0.5 bg-black-200 " />
         </div>
         <div className="flex flex-col items-center pt-5 gap-5">
@@ -76,10 +76,10 @@ const Verhuren = () => {
             </p>
             <FileUpload />
           </div>
-          <div className="w-3/4">
-            <p className="font-bold ">Kies je soort aanhanger:</p>
+          <div className="w-3/4 ">
+            <p className="font-bold">Kies je soort aanhanger:</p>
             <Autocomplete
-              className="w-full buurbak-light border-primary-100 rounded border-1"
+              className="w-full buurbak-light mt-5 border-primary-100 rounded border-1"
               placeholder="Soort..."
             >
               {soortAanhanger.map((item, index) => (
@@ -106,7 +106,7 @@ const Verhuren = () => {
               Kies de accesoires die je bij je aanhanger wilt verhuren:
             </p>
             <Autocomplete
-              className="w-full buurbak-light border-primary-100 rounded border-1"
+              className="w-full buurbak-light mt-5 border-primary-100 rounded border-1"
               placeholder="Accesoires..."
             >
               {accesoires.map((item, index) => (
@@ -120,17 +120,17 @@ const Verhuren = () => {
               ))}
             </Autocomplete>
           </div>
-          <div className="w-3/4">
+          <div className="w-3/4 gap-5">
             <p className="font-bold">
               Kies de locatie waar je je aanhanger vanaf verhuurd:
             </p>
           </div>
-          <div className="w-3/4">
+          <div className="w-3/4 gap-5">
             <p className="font-bold">
               Kies het soort rijbewijs wat vereist is:
             </p>
             <Autocomplete
-              className="w-full buurbak-light border-primary-100 rounded border-1"
+              className="w-full buurbak-light mt-5 border-primary-100 rounded border-1"
               placeholder="Rijbewijs..."
             >
               {license.map((item, index) => (
@@ -224,7 +224,65 @@ const Verhuren = () => {
           </div>
         </div>
       </div>
-      <div className="w-1/3 bg-offWhite-100 min-h-screen"></div>
+
+      <div className="w-1/3 bg-offWhite-100 min-h-screen p-5">
+        <div className="flex flex-row gap-1">
+          <img
+            src="/img/verhuurfoto.png"
+            alt="Verhuurde Aanhanger"
+            className="w-2/4 h-auto rounded-lg"
+          />
+          <div className="w-2/4 flex flex-col gap-1">
+            <div className=" w-2/4 flex flex-row gap-1">
+              <img
+                src="/img/verhuurfoto.png"
+                alt="Verhuurde Aanhanger"
+                className="w-full h-auto rounded-lg"
+              />{" "}
+              <img
+                src="/img/verhuurfoto.png"
+                alt="Verhuurde Aanhanger"
+                className="w-full h-auto rounded-lg"
+              />{" "}
+            </div>
+            <div className="w-2/4 flex flex-row gap-1">
+              <img
+                src="/img/verhuurfoto.png"
+                alt="Verhuurde Aanhanger"
+                className="w-full h-auto rounded-lg"
+              />{" "}
+              <img
+                src="/img/verhuurfoto.png"
+                alt="Verhuurde Aanhanger"
+                className="w-full h-auto rounded-lg"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-2">
+          <h6 className="flex flex-row text-primary-100 font-bold">
+            Gesloten Aanhanger
+          </h6>
+          <p className="text-gray-100">Omschrijving:</p>
+        </div>
+        <div className="flex flex-col p-2 gap-3 items-center">
+          <hr className="w-full h-0.5 bg-black-100 "></hr>
+          <h4 className="font-bold">Locatie</h4>
+          <h6>Hilversum</h6>
+          <h4 className="font-bold">Prijs</h4>
+          <h6>€100 per dag</h6>
+          <hr className="w-full h-0.5 bg-black-100 "></hr>
+        </div>
+        <div className="flex mt-2 justify-center">
+          <h4 className="font-bold">Details</h4>
+        </div>
+        <div className="flex flex-col justify-start"></div>
+        <p className="font-bold mt-5">Benodigd Rijbewijs</p>
+        <div className="flex flex-row">
+          <p className="bg-gray-100 mt-3">Rijbewijs</p>
+          <p className="mt-3 bg-gray-100 pl-20">B</p>
+        </div>
+      </div>
     </div>
   );
 };
