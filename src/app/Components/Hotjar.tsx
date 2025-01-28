e169ea765a04bc606a745c835db834604e144a88
Luuk van Dijk <luukvandijk2003@gmail.com>
2025-01-28 14:53:12 +0100

    removed console.log

--- src/app/Components/Hotjar.tsx
+++ src/app/Components/Hotjar.tsx
@@ -5,7 +5,6 @@ import { useEffect } from "react";
 const Hotjar = () => {
   useEffect(() => {
     // Hotjar Initialisatie
-    console.log("Initializing Hotjar...");
     try {
       (function (h: any, o: any, t: any, j: any, a?: any, r?: any) {
         h.hj =
@@ -20,13 +19,11 @@ const Hotjar = () => {
         r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
         a.appendChild(r);
       })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
-      console.log("Hotjar initialized successfully.");
     } catch (error) {
       console.error("Error initializing Hotjar:", error);
     }
 
     // Microsoft Clarity Initialisatie
-    console.log("Initializing Microsoft Clarity...");
     try {
       (function (c: any, l: any, a: any, r: any, i?: any, t?: any, y?: any) {
         c[a] =
@@ -40,13 +37,11 @@ const Hotjar = () => {
         y = l.getElementsByTagName(r)[0];
         y.parentNode?.insertBefore(t, y);
       })(window, document, "clarity", "script", "prudemtx5m");
-      console.log("Microsoft Clarity initialized successfully.");
     } catch (error) {
       console.error("Error initializing Microsoft Clarity:", error);
     }
 
     // Cookiebot Initialisatie
-    console.log("Initializing Cookiebot...");
     try {
       const script = document.createElement("script");
       script.id = "Cookiebot";
@@ -54,8 +49,6 @@ const Hotjar = () => {
       script.setAttribute("data-cbid", "64138b24-59cf-4e36-891f-cd08c83ba9f7");
       script.type = "text/javascript";
       document.head.appendChild(script);
-
-      console.log("Cookiebot initialized successfully.");
     } catch (error) {
       console.error("Error initializing Cookiebot:", error);
     }
