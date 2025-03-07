7fe75789db752ae5bc4c0969b3923c7be7ff97e1
HunterRvB <114484205+HunterRvB@users.noreply.github.com>
2025-03-07 12:41:24 +0100

    v2 reviewpage

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -1,20 +1,56 @@
+"use client";
+
 import { Star } from "lucide-react";
+import Button from "../Components/Button";
+import { useState, useEffect } from "react";
+import LogoColor from "../Assets/horizontalColorLogo.svg";
+import Image from "next/image";
+import ProfileDisplay from "../Components/AanbodItem/ProfileDisplay";
+import { TrailerData } from "@/app/Types/Reservation";
+import { getTrailer } from "@/app/api/Trailer-controller";
 
+const ReviewPage = ({ params }: { params: { AanbodId: string } }) => {
+  const [trailerOffer, setTrailerOffer] = useState<TrailerData>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getTrailer(params.AanbodId);
+        setTrailerOffer(data);
+        setLoading(false);
+      } catch (error: any) {
+        console.warn(error.message);
+      }
+    };
 
-const ReviewPage: React.FC = () => {
-    return (
-        <main className="flex items-center justify-center min-h-screen bg-gray-50">
-        <h2>Laat een review achter voor jouw gehuurde aanhanger</h2>
-        
-        <button
-            type="submit"
-            className="w-full py-2 px-4 bg-primary-100 text-white font-semibold rounded-md hover:bg-primary-200 focus:outline-none focus:ring-2 focus:ring-primary-200 focus:ring-opacity-50"
-          >
-            Verstuur
-        </button>        
-        </main>
-    );
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  return (
+    <main className="w-dvw h-dvh flex flex-col justify-center items-center">
+      <h2 className="text-center">
+        Laat een review achter voor jouw gehuurde aanhanger
+      </h2>
+      <div className="w-max h-60 flex flex-row justify-center items-center">
+        <Image alt="Buurbak logo" src={LogoColor} className={"w-max h-max"} />
+        <div className="w-max h-max flex flex-col justify-center">
+          <div className="bg-gray-200 w-full h-fit"></div>
+          <div></div>
+        </div>
+      </div>
+
+      <Button label="Verstuur" submit type="primary" />
+    </main>
+  );
 };
 
 export default ReviewPage;
