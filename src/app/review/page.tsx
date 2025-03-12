5cc148c976504e17d6f8864a59d0d289992d37fa
HunterRvB <114484205+HunterRvB@users.noreply.github.com>
2025-03-12 11:08:41 +0100

    Added text input and fixed some responsiveness issues

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import ProfileDisplay from "../Components/AanbodItem/ProfileDisplay";
 import { TrailerData } from "@/app/Types/Reservation";
 import { getTrailer } from "@/app/api/Trailer-controller";
+import InputField from "../Components/InputField";
 
 const ReviewPage = ({ params }: { params: { AanbodId: string } }) => {
   const [trailerOffer, setTrailerOffer] = useState<TrailerData>();
@@ -36,18 +37,18 @@ const ReviewPage = ({ params }: { params: { AanbodId: string } }) => {
   }
   return (
     <main className=" sm:pt-6 flex flex-col items-center mt-[110px]">
-      <h2 className="text-center">
+      <h2 className="text-center text-h5 md:text-h2 h-fit m-3">
         Laat een review achter voor jouw gehuurde aanhanger
       </h2>
       {/* {trailerOffer && ( */}
       {/* <> */}
       <div className="w-[80dvw] h-60 flex flex-col md:flex-row justify-evenly items-center p-3">
         <Image alt="Buurbak logo" src={LogoColor} className={"w-max h-max"} />
-        <div className="h-full flex flex-col justify-evenly">
+        <div className="h-full flex flex-col justify-evenly items-center md:items-start">
           <div>
-            {/* <ProfileDisplay trailerOffer={trailerOffer} /> */} <p>Jippie</p>
+            {/* <ProfileDisplay trailerOffer={trailerOffer} /> */} <p>Jan de Boom</p>
           </div>
-          <h2 className="text-primary-100 text-h4 ">
+          <h2 className="text-primary-100 text-h4 text-center md:text-left">
             {/* {trailerOffer.trailer_type} */} Gesloten Aanhanger
           </h2>
         </div>
@@ -55,8 +56,17 @@ const ReviewPage = ({ params }: { params: { AanbodId: string } }) => {
       {/* </> */}
       {/* )} */}
       <StarRating />
+      <InputField
+            type="text"
+            // pattern="^(?:[A-Z]|[a-z])[a-z ]+(?: [A-Z]?[a-z ]*)*$"
+            className=" md:w-[50dvw] w-[80dvw]  m-5"
+            label="Type hier je review..."
+            inputType="text"
+            outline={true}
+            required
+            />
 
-      <Button label="Verstuur" submit type="primary" />
+      <Button label="Verstuur" submit type="primary" className="m-5" />
     </main>
   );
 };
