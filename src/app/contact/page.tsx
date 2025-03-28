f863ce3211544da7e7ccb9495c6e2b01587f8a41
Sophrania0mori <lisenca.vanweerden@student.hu.nl>
2025-03-28 11:18:27 +0100

    colour fix 2.0

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -112,7 +112,7 @@ const ContactPage: React.FC = () => {
         </form>
       </div>
       <div className="flex flex-col justify-evenly items-center p-8 bg-primary-100 shadow-lg w-screen mt-auto h-[35vh]">
-        <h2 className="text-2xl font-semibold text-center text-gray-800">
+        <h2 className="text-2xl font-semibold text-center text-white">
           Schade? Meld je schade!
         </h2>
         <div className=" flex flex-row space-x-20">
