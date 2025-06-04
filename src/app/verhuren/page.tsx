c13d9b8cbd9d0abbe7cb6cbb69d679c16e88e8ba
Robin <114484205+HunterRvB@users.noreply.github.com>
2025-06-04 18:04:00 +0200

    Update page.tsx
    Changed the desc for location for better and clearer userflow

--- src/app/verhuren/page.tsx
+++ src/app/verhuren/page.tsx
@@ -341,8 +341,7 @@ const Verhuren = () => {
           </div>
           <div className="w-3/4 gap-5">
             <p className="font-bold">
-              Kies de locatie waar je je aanhanger vanaf verhuurd (bijv.
-              Kamperbinnenpoort 1, Utrecht, Netherlands):
+              Kies de locatie waar je je aanhanger vanaf verhuurd:
             </p>
             <LocationInput
               onLocationChange={handleLocationChange}
