c150b5f198a39ba659aca175b3ecf7389604137a
jessekoekkoek <jesse.koekkoek@navara.nl>
2025-06-09 10:04:39 +0200

    fix(Navbar): Fix Navbar desktop height and simplify component

--- src/app/Components/Landing.tsx
+++ src/app/Components/Landing.tsx
@@ -3,10 +3,22 @@
 import { CircleCheckBig } from "lucide-react";
 import Link from "next/link";
 import Button from "./Button";
+import Image from "next/image";
 
 export default function Landing() {
   return (
-    <div className="flex justify-center items-center h-screen w-full bg-landing-background bg-cover bg-center overflow-hidden">
+    <div className="flex justify-center items-center h-screen w-full overflow-hidden">
+      {/* Background Image */}
+      <div className="absolute inset-0">
+        <Image
+          src="/img/BuurBak_Duurzaam-Delen_Aanhanger02972022_1920_1.webp" // Path to your hero image in the public folder
+          alt="Hero Background"
+          fill // This makes the image fill the parent div
+          style={{ objectFit: 'cover' }} // Ensures the image covers the area without distortion
+          priority // Preloads the image as it's above the fold
+          quality={80} // Adjust quality for optimization
+        />
+      </div>
       <div className="flex justify-center items-center flex-col w-full h-full backdrop-brightness-50">
         <h1 className="hidden md:flex text-white w-5/12 text-center font-sans font-bold">
           Huur en verhuur je aanhanger via BuurBak
@@ -31,7 +43,7 @@ export default function Landing() {
           </div>
           <div className="flex justify-between mt-12 w-fit">
             <Link href={"/aanbod"}>
-              <Button label="Bekijk het aanbod" className="w-[246px]" />
+              <Button label="Bekijk het aanbod" className="w-60" />
             </Link>
           </div>
         </div>
--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -2,10 +2,7 @@
 
 import {
   Car,
-  CircleUserRound,
-  Facebook,
-  Home,
-  Linkedin,
+  LucideIcon,
   Mail,
   Menu,
   Tag,
@@ -13,88 +10,59 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import LogoWhite from "../Assets/Frame.svg";
 import LogoColor from "../Assets/horizontalColorLogo.svg";
 import { PlateauTrailer } from "../icons/TrailerIcons";
 
-import { getSignedInUserOrUndefined } from "@/lib/authUtil";
-import { usePathname } from "next/navigation";
-import { hasToken } from "../../lib/cookieUtil";
-import { UserDetails } from "../Types/User";
 import Authentication from "./Authentication";
 
-const Links = [
-  { name: "Home", url: "/", icon: Home },
-  { name: "Aanbod", url: "/aanbod" },
-  { name: "Ik wil verhuren", url: "/verhuren" },
-  { name: "Over ons", url: "/over_ons" },
-  { name: "Contact", url: "/contact" },
-];
-
-const MobileLinks = [
-  { name: "Home", url: "/", icon: Home },
-  { name: "Aanbod", url: "/aanbod", icon: PlateauTrailer },
-  { name: "Verhuren", url: "/verhuren", icon: Tag },
-  { name: "Over ons", url: "/over_ons", icon: Car },
-  { name: "Contact", url: "/contact", icon: Mail },
-];
+interface LinkData {
+  name: string;
+  url: string;
+  icon: LucideIcon | Function;
+}
 
 const Navbar = () => {
+  const mobileLinks: LinkData[] = [
+    { name: "Aanbod", url: "/aanbod", icon: PlateauTrailer },
+    { name: "Verhuren", url: "/verhuren", icon: Tag },
+    { name: "Over ons", url: "/over_ons", icon: Car },
+    { name: "Contact", url: "/contact", icon: Mail },
+  ];
   const [open, setOpen] = useState(false);
-  const [signedIn, setSignedIn] = useState(false); // Corrected typo
   const [scrolled, isScrolled] = useState(true);
-  const [user, setUser] = useState<UserDetails>();
 
-  const currentRoute = usePathname();
-
-  useEffect(() => {
-    const checkToken = async () => {
-      if (await hasToken("sb-tnffbjgnzpqsjlaumogv-auth-token")) {
-        setSignedIn(true); // Corrected typo
-        const getApi = async () => {
-          const userData = await getSignedInUserOrUndefined();
-          setUser(userData);
-        };
-        getApi();
-      } else {
-        setSignedIn(false); // Corrected typo
-      }
-    };
-
-    checkToken();
-  }, [open]);
+  const getLinkElement = (link: LinkData) => {
+    return (
+      <Link className="text-secondary-100 mx-2" href={link.url}>{link.name}</Link>
+    );
+  };
 
   return (
-    <main>
+    <>
       <div
-        className={`fixed top-0 w-full z-50 ${
-          scrolled ? "bg-white" : "bg-none"
-        }`}
+        className={`fixed top-0 w-full z-50 min-h-16 p-4 flex items-center justify-between ${scrolled ? "bg-white" : "bg-none"
+          }`}
       >
-        <div className="md:flex justify-between items-center md:px-10 py-4 px-7">
-          {/*logo*/}
-          <Link href="/">
-            {scrolled ? (
-              <Image
-                alt="Buurbak logo"
-                src={LogoColor}
-                className={(open ? "hidden " : "") + "w-fit h-fit"}
-              />
-            ) : (
-              <Image
-                alt="Buurbak logo"
-                src={LogoWhite}
-                className={(open ? "hidden " : "") + "w-fit h-fit"}
-              />
-            )}
-          </Link>
+        {/*logo*/}
+        <Link href="/">
+          {scrolled ? (
+            <Image
+              alt="Buurbak logo"
+              src={LogoColor}
+            />
+          ) : (
+            <Image
+              alt="Buurbak logo"
+              src={LogoWhite}
+            />
+          )}
+        </Link>
 
-          <div
+        <div>
+          <div className="md:hidden"
             onClick={() => setOpen(!open)}
-            className={
-              "w-7 h-7 absolute right-8 top-6 cursor-pointer md:hidden"
-            }
           >
             {open ? (
               <X size={36} />
@@ -102,77 +70,29 @@ const Navbar = () => {
               <Menu color={`${scrolled ? "black" : "white"}`} size={36} />
             )}
           </div>
-
-          {/*Navbar*/}
           <div>
-            <ul
-              className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-50 z-[-1] top-0 h-screen md:h-fit bg-white md:bg-transparent md:w-auto md:pl-0 px-9 md:pr-0 pt-20 md:pt-0 transition-all duration-50 ease-in ${
-                open ? "right-0 w-11/12" : "right-[-500px]"
-              }`}
-            >
-              {open
+            {mobileLinks.map((link) => getLinkElement(link))}
+            <Authentication />
+          </div>
+        </div>
+        {/* {open
                 ? MobileLinks.map((link, index) => (
-                    <li
-                      className={`flex flex-row gap-4 align-center py-4 md:my-0 md:ml-8 text-lg font-semibold border-b-1 border-b-offWhite-100 md:border-0 ${
-                        link.url === currentRoute && "text-primary-100"
+                  <li
+                    className={`flex flex-row gap-4 align-center py-4 md:my-0 md:ml-8 text-lg font-semibold border-b-1 border-b-offWhite-100 md:border-0 ${link.url === currentRoute && "text-primary-100"
                       }`}
-                      key={index}
-                    >
-                      <link.icon size={32} />
-                      <a className="mt-1" href={link.url}>
-                        {link.name}
-                      </a>
-                    </li>
-                  ))
-                : Links.map((link, index) => (
-                    <li
-                      className={`py-4 md:my-0 md:ml-8 ${
-                        scrolled ? "text-secondary-100" : "text-white"
-                      } ${
-                        link.name.includes("Ik wil verhuren") &&
-                        "md:bg-primary-100 md:px-4 md:py-2 md:rounded text-white"
-                      }`}
-                      key={index}
-                    >
-                      {link.name === "Inloggen" && signedIn ? ( // Corrected typo
-                        user && user.profile_picture ? (
-                          <Link href="/dashboard">
-                            <div className="w-14 h-14 relative">
-                              <Image
-                                src={
-                                  user.profile_picture &&
-                                  user.profile_picture.trim().length
-                                    ? user.profile_picture
-                                    : "/default-profile.png"
-                                }
-                                alt="User profile picture"
-                                fill
-                                sizes="100% 100%"
-                                priority={true}
-                                className="rounded-full object-cover"
-                              />
-                            </div>
-                          </Link>
-                        ) : (
-                          <Link href="/dashboard">
-                            <CircleUserRound className="w-auto h-12" />
-                          </Link>
-                        )
-                      ) : (
-                        <a className="cursor-pointer" href={link.url}>
-                          {link.name}
-                        </a>
-                      )}
-                    </li>
-                  ))}
-              <li>
-                {/* TODO: Doesn't have a mobile menu equivalent anymore right now. This will be fixed in a future MR. */}
-                <Authentication />
-              </li>
-              <div
-                className={`flex flex-col mt-20 items-center ${
-                  open ? "" : "hidden"
-                }`}
+                    key={index}
+                  >
+                    <link.icon size={32} />
+                    <a className="mt-1" href={link.url}>
+                      {link.name}
+                    </a>
+                  </li>
+                ))
+                : Links.map((link, index) => ( */}
+
+        {/* <div
+                className={`flex flex-col mt-20 items-center ${open ? "" : "hidden"
+                  }`}
               >
                 <p className="text-xl text-primary-100 font-semibold mb-6">
                   Volg ons
@@ -193,13 +113,41 @@ const Navbar = () => {
                     <Facebook className="h-8 w-8" />
                   </a>
                 </div>
-              </div>
-            </ul>
-          </div>
-        </div>
+              </div> */}
+        {/*Navbar*/}
+        {/* {mobileLinks.map((link) => {
+              <Link>link.</Link>;
+
+            })
+            } */}
+        {/* <ul
+              className={`flex items-center md:pb-0 pb-12 absolute md:static md:z-50 z-[-1] top-0 h-screen md:h-fit bg-white md:bg-transparent md:w-auto md:pl-0 px-9 md:pr-0 pt-20 md:pt-0 transition-all duration-50 ease-in ${open ? "right-0 w-11/12" : "right-[-500px]"
+                }`}
+            > */}
+        {/* <li
+                className={`py-4 md:my-0 mx-4 ${scrolled ? "text-secondary-100" : "text-white"
+                  } ${link.name.includes("Ik wil verhuren") &&
+                  "md:bg-primary-100 md:px-4 md:py-2 md:rounded text-white"
+                  }`}
+                key={index}
+              >
+                <a
+                  className="cursor-pointer"
+                  // onClick={
+                  //   link.name === "Inloggen" ? onOpen : undefined
+                  // }
+                  href={link.url}
+                >
+                  {link.name}
+                </a>
+              </li>
+              <li> */}
+        {/* TODO: Doesn't have a mobile menu equivalent anymore right now. This will be fixed in a future MR. */}
+        {/* </li>
+            </ul> */}
       </div>
-    </main>
+    </>
   );
 };
 
-export default Navbar;
+export default Navbar;;
