cd4e419543b0628f327a3dbbc1cc59c3546d0b8f
Luuk van Dijk <luukvandijk2003@gmail.com>
2025-10-09 14:52:54 +0200

    fixed some design issues and now on mobile no map visble for cleaner look

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Facebook, Linkedin, Instagram } from "lucide-react";
+import { Facebook, Instagram, Linkedin } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -10,14 +10,16 @@ const Footer = () => {
   const currentRoute = usePathname();
 
   return (
-    <footer className="w-full bg-[#f7f7f7] border-t border-gray-200 pt-8 mt-12">
+    <footer className="w-full bg-[#f7f7f7] border-t border-gray-200 pt-8">
       {/* Bovenste deel */}
       <div className="w-full flex flex-col lg:flex-row justify-between items-start lg:items-center gap-y-10 px-6 xl:px-28 2xl:px-52 pb-8 max-sm:items-center max-sm:justify-center max-sm:min-h-[300px]">
         {/* Navigatie Links */}
         <div className="flex flex-1 justify-center lg:justify-start items-start order-2 lg:order-1 max-sm:justify-center">
           <div className="flex flex-wrap justify-start gap-20 min-w-[260px] max-w-[600px] max-sm:flex-col max-sm:gap-6 max-sm:text-center max-sm:items-center max-sm:justify-center">
             <div className="max-sm:text-center">
-              <p className="text-primary-100 font-bold text-sm mb-2.5">AANBOD</p>
+              <p className="text-primary-100 font-bold text-sm mb-2.5">
+                AANBOD
+              </p>
               <p className="mt-1.5 hover:font-medium">
                 <Link
                   href="/aanbod"
@@ -28,7 +30,9 @@ const Footer = () => {
               </p>
             </div>
             <div className="max-sm:text-center">
-              <p className="text-primary-100 font-bold text-sm mb-2.5">VERHUREN</p>
+              <p className="text-primary-100 font-bold text-sm mb-2.5">
+                VERHUREN
+              </p>
               <p className="mt-1.5 hover:font-medium">
                 <Link
                   href="/verhuren"
@@ -39,7 +43,9 @@ const Footer = () => {
               </p>
             </div>
             <div className="max-sm:text-center">
-              <p className="text-primary-100 font-bold text-sm mb-2.5">CONTACT</p>
+              <p className="text-primary-100 font-bold text-sm mb-2.5">
+                CONTACT
+              </p>
               <p className="mt-1.5 hover:font-medium">
                 <Link
                   href="/contact"
@@ -58,13 +64,22 @@ const Footer = () => {
           </Link>
           <p className="text-sm text-primary-100">Duurzaam door te delen</p>
           <div className="flex items-center mt-2">
-            <a href="https://www.facebook.com/BuurBak" className="w-[42px] h-[42px] rounded-full bg-[#f1f1f1] mr-2.5 flex items-center justify-center text-xl text-gray-600 border hover:bg-primary-100 hover:text-white transition-all duration-300">
+            <a
+              href="https://www.facebook.com/BuurBak"
+              className="w-[42px] h-[42px] rounded-full bg-[#f1f1f1] mr-2.5 flex items-center justify-center text-xl text-gray-600 border hover:bg-primary-100 hover:text-white transition-all duration-300"
+            >
               <Facebook size={20} />
             </a>
-            <a href="https://www.linkedin.com/company/buurbak/" className="w-[42px] h-[42px] rounded-full bg-[#f1f1f1] mr-2.5 flex items-center justify-center text-xl text-gray-600 border hover:bg-primary-100 hover:text-white transition-all duration-300">
+            <a
+              href="https://www.linkedin.com/company/buurbak/"
+              className="w-[42px] h-[42px] rounded-full bg-[#f1f1f1] mr-2.5 flex items-center justify-center text-xl text-gray-600 border hover:bg-primary-100 hover:text-white transition-all duration-300"
+            >
               <Linkedin size={20} />
             </a>
-            <a href="https://www.instagram.com/buurbak/" className="w-[42px] h-[42px] rounded-full bg-[#f1f1f1] mr-2.5 flex items-center justify-center text-xl text-gray-600 border hover:bg-primary-100 hover:text-white transition-all duration-300">
+            <a
+              href="https://www.instagram.com/buurbak/"
+              className="w-[42px] h-[42px] rounded-full bg-[#f1f1f1] mr-2.5 flex items-center justify-center text-xl text-gray-600 border hover:bg-primary-100 hover:text-white transition-all duration-300"
+            >
               <Instagram size={20} />
             </a>
           </div>
