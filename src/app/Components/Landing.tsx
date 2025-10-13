b3037a6c074e4f76fbda21f7a32e67f39f31c075
Luuk van Dijk <luukvandijk2003@gmail.com>
2025-10-13 17:46:21 +0200

    fixed white space on home page

--- src/app/Components/Landing.tsx
+++ src/app/Components/Landing.tsx
@@ -1,22 +1,22 @@
 "use client";
 
 import { CircleCheckBig } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
-import Image from "next/image";
 
 export default function Landing() {
   return (
-    <div className="flex justify-center items-center h-screen w-full overflow-hidden">
+    <div className="flex justify-center items-center h-[calc(100dvh-4rem)] w-full overflow-hidden">
       {/* Background Image */}
       <div className="absolute inset-0">
         <Image
-          src="/img/BuurBak_Duurzaam-Delen_Aanhanger02972022_1920_1.webp" // Path to your hero image in the public folder
+          src="/img/BuurBak_Duurzaam-Delen_Aanhanger02972022_1920_1.webp"
           alt="Hero Background"
-          fill // This makes the image fill the parent div
-          style={{ objectFit: 'cover' }} // Ensures the image covers the area without distortion
-          priority // Preloads the image as it's above the fold
-          quality={80} // Adjust quality for optimization
+          fill
+          style={{ objectFit: "cover" }}
+          priority
+          quality={80}
         />
         <div className="flex justify-center items-center flex-col w-full h-full backdrop-brightness-50">
           <h1 className="hidden md:flex text-white w-5/12 text-center font-sans font-bold">
